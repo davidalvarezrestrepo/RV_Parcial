b1adb202fee38760ee977ad9471473767795f22a
davidalvarezrestrepo <david.alvarez_res@uao.edu.co>
2025-05-28 16:08:35 -0500

    s

---
+++ EmpezarACA/Assets/Scripts/Gun.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Gun : MonoBehaviour
+{
+    public XRGrabInteractable grabInteract;
+
+    public DisparoRay disparo;
+
+    public GameObject shootFX;
+
+    void Start()
+    {
+        grabInteract.activated.AddListener(x => Disparando());
+        grabInteract.deactivated.AddListener(x => DejarDisparo());
+    }
+
+    public void Disparando()
+    {
+        disparo.Disparar();
+    }
+
+    public void DejarDisparo()
+    {
+        //Espacio para efectos al dejar de disparar
+    }
+}
